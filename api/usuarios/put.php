669a5f5d2a72037dd01c5675122c34ff6b02b2e7
Guilherme0112 <guimendesmen124@gmail.com>
2024-07-29 19:23:11 -0300

    commit

--- api/usuarios/put.php
+++ api/usuarios/put.php
@@ -4,25 +4,30 @@
         $mensagem = array();
         $db = DB::connect();
         try{
-            if(strlen($_POST['nome']) > 3 && strlen($_POST['nome']) < 50){
-                $sql = $db->prepare("UPDATE usuarios SET nome = '" . $_POST['nome'] . "' WHERE id = $parametro");
-                $sql->execute();
-                $mensagem[] = 'nome';
-            }
-            if(strlen($_POST['senha'] > 4 && strlen($_POST['senha']) < 16)){
-                $sql = $db->prepare("UPDATE usuarios SET senha = '" . $_POST['senha'] . "' WHERE id = $parametro");
-                $sql->execute();
-                $mensagem[] = 'senha';
-            }
-            if(strlen($_POST['telefone']) === 15){
-                $sql = $db->prepare("UPDATE usuarios SET telefone = '" . $_POST['telefone'] . "' WHERE id = $parametro");
-                $sql->execute();
-                $mensagem[] = 'telefone';
-            }
-            if(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
-                $sql = $db->prepare("UPDATE usuarios SET email = '" . $_POST['email'] . "' WHERE id = $parametro");
-                $sql->execute();
-                $mensagem[] = 'email';
+            if(isset($_POST['nome']) || isset($_POST['senha']) || isset($_POST['telefone']) || isset($_POST['email'])){
+                if(isset($_POST['nome']) && strlen($_POST['nome']) > 3 && strlen($_POST['nome']) < 50){
+                    $sql = $db->prepare("UPDATE usuarios SET nome = '" . $_POST['nome'] . "' WHERE id = $parametro");
+                    $sql->execute();
+                    $mensagem[] = 'nome';
+                }
+                if(isset($_POST['senha']) && strlen($_POST['senha'] > 4 && strlen($_POST['senha']) < 16)){
+                    $sql = $db->prepare("UPDATE usuarios SET senha = '" . $_POST['senha'] . "' WHERE id = $parametro");
+                    $sql->execute();
+                    $mensagem[] = 'senha';
+                }
+                if(isset($_POST['telefone']) && strlen($_POST['telefone']) === 15){
+                    $sql = $db->prepare("UPDATE usuarios SET telefone = '" . $_POST['telefone'] . "' WHERE id = $parametro");
+                    $sql->execute();
+                    $mensagem[] = 'telefone';
+                }
+                if(isset($_POST['email']) && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+                    $sql = $db->prepare("UPDATE usuarios SET email = '" . $_POST['email'] . "' WHERE id = $parametro");
+                    $sql->execute();
+                    $mensagem[] = 'email';
+                }
+            } else {
+                echo json_encode(["Erro" => "É preciso preencher algum campo para editar os dados"]);
+                exit;
             }
 
             // mensagem caso dê certo
@@ -32,7 +37,7 @@
                 $msg .= " $palavra,"; 
             }
             $msg = substr($msg, 0, -1);
-            echo json_encode(["Sucesso" => "Os campos $msg foram modificados"]);
+            echo json_encode(["Sucesso" => "O(s) campo(s)$msg foram modificados"]);
 
         } catch (PDOException $e){
             echo json_encode(["Erro" => $e->getMessage()]);
--- api/usuarios/usuarios.php
+++ api/usuarios/usuarios.php
@@ -1,16 +1,20 @@
 <?php
     if($api == 'usuarios'){
-        if($method == 'GET'){
-            include_once "get.php";
-        }
-        if($method == 'POST' && empty($_POST['_method'])){
-            include_once "post.php";
-        }
-        if($method == 'POST' && $_POST['_method'] == 'PUT'){
-            include_once "put.php";
-        }
-        if($method == 'POST' && $_POST['_method'] == 'DELETE'){
-            include_once "delete.php";
+        try{
+            if($method == 'GET'){
+                include_once "get.php";
+            }
+            if($method == 'POST' && empty($_POST['_method'])){
+                include_once "post.php";
+            }
+            if($method == 'POST' && $_POST['_method'] == 'PUT'){
+                include_once "put.php";
+            }
+            if($method == 'POST' && $_POST['_method'] == 'DELETE'){
+                include_once "delete.php";
+            }
+        } catch (Exception $e){
+            echo json_encode(["Erro" => $e->getMessage()]);
         }
     }
 ?>
